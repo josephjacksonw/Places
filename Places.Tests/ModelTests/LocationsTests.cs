dc8a350ce22d05fce330b17a9748a0f1004cf525
josephjacksonw <josephjacksonw@gmail.com>
2022-09-28 16:48:05 -0400

    tests 3,4,5 good fail
    Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>

--- Places.Tests/ModelTests/LocationsTests.cs
+++ Places.Tests/ModelTests/LocationsTests.cs
@@ -23,5 +23,53 @@ namespace Places.TestTools
       string result = newLocation.Description;
       Assert.AreEqual(description, result);
     }
+    //----------------------------------------------
+
+    [TestMethod]
+    public void SetDescription_UpdateDescription_String()
+    {
+      //Arrange
+      string description = "this is a test description.";
+      Location newLocation = new Location(description);
+
+      //Act
+      string updatedDescription = "an updated description";
+      newLocation.Description = updatedDescription;
+      string result = "good fail";//newLocation.Description;
+
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_LocationList()
+    {
+      // Arrange
+      List<Location> newList = new List<Location> { };
+
+      // Act
+      List<Location> result = Location.GetAll();
+      Location newLocation = new Location("so that the newList list isn't empty and good fails");
+
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsLocations_LocationList()
+    {
+      //Arrange
+      string description01 = "France";
+      string description02 = "Germany";
+      Location newLocation1 = new Location(description01);
+      Location newLocation2 = new Location(description02);
+      List<Location> newList = new List<Location> { newLocation1, newLocation2 };
+
+      //Act
+      List<Location> result = Location.GetAll();
+      Location newLocation3 = new Location("3rd thing so we have a good fail");
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
